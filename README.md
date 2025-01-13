a834b150c558be61b2b36219bcba98cca4ee211f
Moonlight <senda.kiyoharu.dev@gmail.com>
2025-01-13 22:02:50 +0800

    feat: fist commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align='center'>Senda Kiyoharu Portfolio</h1>
+
+<div align="center">
+   Welcome to my web portfolio! I designed and developed this portfolio to showcase my web development projects and to provide a little more information about myself.
+</div>
+<br/>
+<br/>
+<br/>
+<br/>
+
+💡 Technologies:
+
+<img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React icon" height="30" /> <img src="https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E" alt="JavaScript icon" height="30" /> <img src="https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white" alt="Styles Components icon" height="30" /> <img src="https://img.shields.io/badge/Framer-black?style=for-the-badge&logo=framer&logoColor=blue" height="30"/>
+<img src="https://img.shields.io/badge/i18next-009688?style=for-the-badge&logo=i18next&logoColor=004d40" height="30"/> <img src="https://img.shields.io/badge/Vite-B73BFE?style=for-the-badge&logo=vite&logoColor=FFD62E" height="30"/> <img src="https://img.shields.io/badge/Inkscape-000000?style=for-the-badge&logo=Inkscape&logoColor=white" height="30"/>
+
+<br/>
+
+📷 Questions and Feedback
+
+Feel free to contact me at senda.kiyoharu.dev@gmail.com if you have any questions or feedback. <br>
+You can view more of my projects at https://github.com/magician-00
---
+++ src/data/projectData.js
@@ -0,0 +1,57 @@
+export const projectData = [
+  {
+    id: crypto.randomUUID(),
+    title: 'Touring',
+    description:
+      "This platform simplifies the booking process by providing an intuitive interface for users and robust management tools for tour operators.",
+    technologies: ['Angular', 'SCSS', 'Primeng', 'Nest.js', 'MySQL'],
+    imageUrl: '/rasters/touring.png',
+    mobileUrl: '/rasters/touring.png',
+    repository: 'https://github.com/magician-00/Touring',
+    livelink: '',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'LMSW CheatSheet',
+    description:
+      "This is your overwhelm-free, test-busting study package! In this 5-unit program, we’ve broken down the exam’s formulas through 5 tailored habits with examples, practice questions, and one-on-one aid so you can cut the bullshit, cut distractors, and get the answers!",
+    technologies: ['Next.js', 'Tailwind', 'Sanity', 'Stripe'],
+    imageUrl: 'rasters/sashi-care.png',
+    mobileUrl: 'rasters/sashi-care.png',
+    repository: 'https://github.com/magician-00/sashi-care',
+    livelink: '',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'AI Avatar Interaction',
+    description:
+      "Users can easily upload documents, allowing the AI to analyze content and generate relevant questions. The AI engages users by asking insightful questions based on the uploaded document, facilitating deeper understanding and interaction. Users can provide answers to the AI's questions and receive feedback, enhancing the learning experience.",
+    technologies: ['HTML', 'Tailwind', 'OpenAi-API', 'AI-Assistant'],
+    imageUrl: 'rasters/ai-avatar-interaction.png',
+    mobileUrl: 'rasters/ai-avatar-interaction.png',
+    repository: 'https://github.com/magician-00/ai-avatar-interaction',
+    livelink: '',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Jurybook Renamer',
+    description:
+      'A file management tool that automates the process of unzipping, renaming, and re-zipping files. This project was developed to streamline workflows for users who frequently handle large volumes of compressed files.',
+    technologies: ['Electron'],
+    imageUrl: 'rasters/jurybook-renamer.png',
+    mobileUrl: 'rasters/jurybook-renamer.png',
+    repository: 'https://github.com/magician-00/jurybook-renamer',
+    livelink: '',
+  },
+  {
+    id: crypto.randomUUID(),
+    title: 'Touring Blog',
+    description:
+      'Developed an intuitive layout that enhances user navigation and encourages engagement with content. Ensured the website is fully responsive, providing an optimal viewing experience across all devices, including desktops, tablets, and smartphones. Integrated a docusaurus-v3 that allows for easy creation, editing, and publishing of blog posts.',
+      technologies: ['React','Docusaurus-v3'],
+      imageUrl: 'rasters/docs.png',
+    mobileUrl: 'rasters/docs.png',
+    repository: 'https://github.com/magician-00/TouringDocs',
+    livelink: '',
+  },
+];
